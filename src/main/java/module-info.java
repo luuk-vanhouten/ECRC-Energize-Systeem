72751d2a9bd5426b465e8e2b0abd4ea565daa0b3
Luuk van Houten <l.vanhouten3@student.han.nl>
2023-03-22 12:43:00 +0100

    Eerste commit test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module nl.saxion.re.ecrcenergizesysteem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens nl.saxion.re.ecrcenergizesysteem to javafx.fxml;
+    exports nl.saxion.re.ecrcenergizesysteem;
+}
