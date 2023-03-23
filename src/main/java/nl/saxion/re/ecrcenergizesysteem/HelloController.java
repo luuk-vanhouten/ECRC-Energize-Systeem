36e2a8b7be10de6d497778dc7060140687c27e83
Luuk van Houten <l.vanhouten3@student.han.nl>
2023-03-23 09:04:44 +0100

    Commit van een aantal beginnende componenten

--- src/main/java/nl/saxion/re/ecrcenergizesysteem/HelloController.java
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/HelloController.java
@@ -1,14 +1,35 @@
 package nl.saxion.re.ecrcenergizesysteem;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick() throws IOException {
+        String componentToOpen = "my-own-component";
+        String windowTitle = "My own component";
+        boolean openInNewWindow = true;
+        int width = 800;
+        int height = 600;
+
+        Stage stage;
+        if (openInNewWindow) {
+            stage = new Stage();
+        } else {
+            stage = (Stage) welcomeText.getScene().getWindow();
+        }
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(componentToOpen + ".fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
+        stage.setTitle(windowTitle);
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/MyOwnComponent.java
@@ -0,0 +1,45 @@
+package nl.saxion.re.ecrcenergizesysteem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import static java.lang.Math.floor;
+
+public class MyOwnComponent {
+    @FXML
+    private Pane panePicture;
+    @FXML
+    private TextField lengthCalculator;
+    @FXML
+    private TextField widthCalculator;
+    @FXML
+    private Label answer;
+
+
+    @FXML
+    private void onCalculateButtonPressed() {
+        Double i = (floor(Double.parseDouble(lengthCalculator.getText()) / 175.4)) * (floor(Double.parseDouble(widthCalculator.getText()) / 109.6));
+        Double j = (floor(Double.parseDouble(lengthCalculator.getText()) / 109.6)) * (floor(Double.parseDouble(widthCalculator.getText()) / 175.4));
+
+        if (i > j) {
+            answer.setText(i + " lengte");
+        } else if (j > i) {
+            answer.setText(j + " breedte");
+        }
+
+        i = 0.0;
+        j = 0.0;
+    }
+
+    @FXML
+    private void onPreviousButtonPressed() {
+
+    }
+
+    @FXML
+    private void onNextButtonPressed() {
+    }
+}
