c7fff57280e5d89ca22b29edd0feeb6fd8502d28
stijnde1e <72652441+stijnde1e@users.noreply.github.com>
2023-03-23 17:03:56 +0100

    Stijn is cool

--- src/main/java/nl/saxion/re/ecrcenergizesysteem/CustomerInformation.java
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/CustomerInformation.java
@@ -1,4 +1,32 @@
 package nl.saxion.re.ecrcenergizesysteem;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CustomerInformation {
+
+    @FXML
+    private TextField firstname;
+    @FXML
+    private TextField lastname;
+    @FXML
+    private TextField placeNumber;
+    @FXML
+    private TextField streetName;
+    @FXML
+    private TextField phoneNumber;
+    @FXML
+    private TextField email;
+    @FXML
+    private Button exit;
+    @FXML
+    private Button next;
+
+@FXML
+    private void onButtonNextPressed(){
+
+
+
+    }
 }
