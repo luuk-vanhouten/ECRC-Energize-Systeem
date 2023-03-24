134e67aef346a28d7c64e8f040206387258664ee
stijnde1e <72652441+stijnde1e@users.noreply.github.com>
2023-03-24 18:53:57 +0100

    Verbetering

--- src/main/java/nl/saxion/re/ecrcenergizesysteem/CustomerInformation.java
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/CustomerInformation.java
@@ -1,8 +1,16 @@
 package nl.saxion.re.ecrcenergizesysteem;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomerInformation {
 
@@ -10,6 +18,8 @@ public class CustomerInformation {
     private TextField firstname;
     @FXML
     private TextField lastname;
+    @FXML
+    private TextField postalCode;
     @FXML
     private TextField placeNumber;
     @FXML
@@ -23,10 +33,21 @@ public class CustomerInformation {
     @FXML
     private Button next;
 
-@FXML
-    private void onButtonNextPressed(){
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
 
+    @FXML
+    Parent root;
 
+    @FXML
+    public void switchToSceneMenuPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("menu-option.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
 }
