730ff0cc5342d16443894c9b197b4f37b45752fb
Luuk van Houten <l.vanhouten3@student.han.nl>
2023-03-28 09:57:18 +0200

    Bijwerken menu-option pagina

--- src/main/java/nl/saxion/re/ecrcenergizesysteem/HelloApplication.java
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends  Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
