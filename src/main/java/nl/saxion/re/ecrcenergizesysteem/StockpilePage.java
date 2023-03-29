e04ad6ac683ea469542b69f5044af7d20d5f9179
Luuk van Houten <l.vanhouten3@student.han.nl>
2023-03-29 11:43:53 +0200

    Bijwerken van alle pagina's zodat deze meer op de wireframes lijken

--- src/main/java/nl/saxion/re/ecrcenergizesysteem/StockpilePage.java
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/StockpilePage.java
@@ -1,4 +1,8 @@
 package nl.saxion.re.ecrcenergizesysteem;
 
+import javafx.event.ActionEvent;
+
 public class StockpilePage {
+    public void saveCustomerInDatabase(ActionEvent actionEvent) {
+    }
 }
