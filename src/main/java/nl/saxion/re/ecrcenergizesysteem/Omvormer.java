8d6ee631e6d4442c89c636be0a03f4d8306c251c
stijnde1e <72652441+stijnde1e@users.noreply.github.com>
2023-03-30 09:06:42 +0200

    offer added

--- src/main/java/nl/saxion/re/ecrcenergizesysteem/Omvormer.java
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/Omvormer.java
@@ -3,15 +3,29 @@ package nl.saxion.re.ecrcenergizesysteem;
 class Omvormer {
     private String name;
     private int maxCapacity;
+    private double price;
+    private int id;
 
-    public Omvormer(String name, int maxCapacity) {
+
+    @Override
+    public String toString() {
+        return  name;
+    }
+
+    public Omvormer(String name, int maxCapacity,double price, int id) {
         this.name = name;
         this.maxCapacity = maxCapacity;
+        this.price=price;
+        this.id=id;
     }
 
     public String getName() {
         return name;
     }
+    public  int getId(){
+        return id;
+    }
+
 
     public int getMaxCapacity() {
         return maxCapacity;
