330a4a99803bea57a1b03de262d27996499e2682
stijnde1e <72652441+stijnde1e@users.noreply.github.com>
2023-04-04 12:52:47 +0200

    offer added

--- src/main/java/nl/saxion/re/ecrcenergizesysteem/HelloApplication.java
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/HelloApplication.java
@@ -6,10 +6,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+
 public class HelloApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Connection conn = Postgres.ConnectionUtil.connectdb();
+
+        Path configFile = Path.of("src/main/resources/dbconfig.properties");
+        if (!Files.exists(configFile)) {
+            System.out.println("Maak een bestand aan met de naam config.txt met het wachtwoord van de database.");
+            System.exit(-1);
+        }
+        String configContents = Files.readString(configFile);
+        System.out.println("Config contents: " + configContents);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("offerTableView.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setScene(scene);
--- src/main/java/nl/saxion/re/ecrcenergizesysteem/Postgres.java
+++ src/main/java/nl/saxion/re/ecrcenergizesysteem/Postgres.java
@@ -1,26 +1,41 @@
 package nl.saxion.re.ecrcenergizesysteem;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Postgres {
 
-    public class ConnectionUtil {
+    public static class ConnectionUtil {
         Connection conn = null;
 
         public static Connection connectdb() {
             Connection conn = null;
             try {
-                String url = "jdbc:postgresql://localhost:5432/Energize ECRC";
-                String user = "postgres";
-                String password = "482148";
-                conn = DriverManager.getConnection(url, user, password);
-                return conn;
+                if (conn == null || conn.isClosed()) {
+                    try (InputStream inputStream = Postgres.class.getResourceAsStream("/dbconfig.properties")) {
+                        Properties properties = new Properties();
+                        properties.load(inputStream);
+
+                        String url = properties.getProperty("db.url");
+                        String user = properties.getProperty("db.username");
+                        String password = properties.getProperty("db.password");
+                        conn = DriverManager.getConnection(url, user, password);
+                        return conn;
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
             } catch (SQLException e) {
                 throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-
+            return conn;
         }
     }
 }
